116edd5a5f7b9238025c8e986f31833574fde997
Babajko <Babajko@i.ua>
2026-04-27 22:29:56 +0300

    disp_drv_sh1107 Update build docker container

--- docker/scripts/build_firmware.sh
+++ docker/scripts/build_firmware.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Disable Kconfig warnings as errors (workaround for ZMK warnings)
+export KCONFIG_WERROR=n
+
 echo "Parsing build.yaml configuration..."
 /usr/local/bin/parse_build_yaml.py --bash > /tmp/build_commands.sh
 chmod +x /tmp/build_commands.sh
